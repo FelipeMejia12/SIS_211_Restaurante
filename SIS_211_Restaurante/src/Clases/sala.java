6df9f5edb2ed4e4aca5315c765b54957d240e0cb
FelipeMejia12 <carlo.mejia@ucb.edu.bo>
2025-12-12 19:43:36 -0400

    Carga Inicial del proyecto desarrollado

---
+++ SIS_211_Restaurante/src/Clases/sala.java
@@ -0,0 +1,42 @@
+package Clases;
+
+public class sala {
+    private int id;
+    private String nombre;
+    private int mesas;
+    
+    public sala(){
+        
+    }
+
+    public sala(int id, String nombre, int mesas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.mesas = mesas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getMesas() {
+        return mesas;
+    }
+
+    public void setMesas(int mesas) {
+        this.mesas = mesas;
+    }
+    
+}
